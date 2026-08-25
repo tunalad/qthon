686bdbb74ff19d669d98fd7ed217ab231cf86983
tunalad <28594843+tunalad@users.noreply.github.com>
2026-08-25 20:52:58 +0200

    fixed versioning script
    program version wasn't using the git hash at all, so this fixes that

--- version.py
+++ version.py
@@ -3,17 +3,12 @@ import subprocess
 __version__ = "0.2.2"
 
 try:
-    subprocess.check_output(["git", "describe", "--tags"], stderr=subprocess.DEVNULL)
-except:
-    try:
-        git_hash = (
-            subprocess.check_output(
-                ["git", "rev-parse", "--short", "HEAD"],
-                stderr=subprocess.DEVNULL,
-            )
-            .decode()
-            .strip()
-        )
-        __version__ = f"{__version__}+{git_hash}"
-    except:
-        pass
+    desc = subprocess.check_output(
+        ["git", "describe", "--tags"],
+        stderr=subprocess.DEVNULL,
+    ).decode().strip()
+
+    if "-" in desc:
+        __version__ = f"{__version__}+{desc.split('-', 1)[1]}"
+except Exception:
+    pass
